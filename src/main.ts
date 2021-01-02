533e33ae45e3403434ff8bc12fe3b7063e3d3161
pan93412 <pan93412@gmail.com>
2021-01-02 21:03:41 +0800

    feat(main): add CORS and rate limit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import * as rateLimit from 'express-rate-limit';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      methods: ['GET', 'POST'],
+    },
+  });
+  app.use(
+    rateLimit({
+      windowMs: 1 * 60 * 1000, // 1 minutes
+      max: 3, // limit each IP to 3 requests per windowMs
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
