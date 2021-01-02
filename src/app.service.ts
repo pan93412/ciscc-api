cdc770eea5b591b534161d1f943a31704a2a1238
pan93412 <pan93412@gmail.com>
2021-01-02 13:17:57 +0800

    feat(app.service): completed getInfo()

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
+import VERSION from './utils/getVersion';
 import { Injectable } from '@nestjs/common';
+import IInfo from './interfaces/IInfo';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getInfo(): IInfo {
+    return {
+      version: VERSION,
+    };
   }
 }
