ec7fe34e3ace1bb89190c7a09a877ac834bd0e1d
pan93412 <pan93412@gmail.com>
2021-01-05 22:00:48 +0800

    fix(main): Preflight response is not successful

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      methods: ['GET', 'POST'],
+      methods: ['GET', 'POST', 'OPTIONS'],
       origin: true,
     },
   });
