1b726c79b15451a10a1db15de733290a621aa780
Yi-Jyun Pan <pan93412@gmail.com>
2021-01-08 16:33:47 +0800

    fix(main): Trust the request of the reverse proxy.
    Fixes #2 .

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import * as rateLimit from 'express-rate-limit';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: {
       methods: ['GET', 'POST', 'OPTIONS'],
       origin: true,
     },
   });
+
+  // rate limit
   app.use(
     rateLimit({
       windowMs: 1 * 60 * 1000, // 1 minutes
-      max: 3, // limit each IP to 3 requests per windowMs
+      max: 12, // limit each IP to 12 requests per windowMs
+               // Everyone can send averagely 1 message in 5 second.
     }),
   );
+  app.set('trust proxy', 1);
   await app.listen(3000);
 }
 bootstrap();
